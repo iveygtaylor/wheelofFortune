fc5e9b563a25830cade288d05a91fe47b93e01cb
Ivey Taylor <iveygtaylor@gmail.com>
2016-11-15 13:55:25 -0500

    <<li><a href="comingevents.html">Events</a></li>li><a href="comingevents.html">Events</a></li>
    		  <<li><a href="comingevents.html">Events</a></li>li><a href="comingevents.html">Events</a></li>

---
+++ wof.js
@@ -0,0 +1,54 @@
+$( document ).ready(function() {
+
+    console.log( "ready!" );
+
+});
+
+var game = ['phrase1', 'phrase2', 'phrase3', 'phrase4', 'phrase5', 'phrase6', 'phrase7', 'phrase8', 'phrase9', 'phrase10'];
+var choice = Math.floor(Math.random()*10);
+var answer = game[choice];
+var myLength = answer.length;
+
+alert(answer);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var phrase1 = "Wisconsin";
+var hint1 = "This midwestern state is known for cheese curds.";
+var phrase2 = "Degas";
+var hint2 = "This French artist is most widely known for his paintings of ballerinas. (HINT: Last name only)";
+var phrase3 = "Corvette";
+var hint3 = "This classic Chevrolet was originally known as the Opel.";
+var phrase4 = "Prince";
+var hint4 = "Name the artist who wrote the Bangles classic hit, 'Manic Monday.'";
+var phrase5 = "Wozniak";
+var hint5 = "Not Jobs. Name the other Steve who founded Apple.";
+var phrase6 = "MySpace";
+var hint6 = "If you took part in this social media site, Tom was probably your first friend.";
+var phrase7 = "Kraftwerk";
+var hint7 = "Name the 70s German electronic act, known for albums 'Computer Love' and 'It's More Fun to Compute,' are still performing 3D concerts."
+var phrase8 = "Avatar";
+var hint8 = "When released in 2009, this movie surpassed 'Titanic' as the all-time highest-grossing movie worldwide."
+var phrase9 = ""
+
+

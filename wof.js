2a0c386e16b2fb520a375dc89dfac1c764154869
Ivey Taylor <iveygtaylor@gmail.com>
2016-11-17 11:23:01 -0500

    Updated with changes from PK and SW--wheel no longer turns and working to add tiles

--- wof.js
+++ wof.js
@@ -1,75 +1,167 @@
+var game;
 $( document ).ready(function() {
-
    console.log( "ready!" );
-
-   setUp();
-
-
+   //chooses phrase
+   //setUp();
+   game = new Game;
+   game.start();
 });
 
-var game = ['ARGUMENT', 'THIS', 'CONCATENATE', 'STRING', 'INTEGER', 'CALLBACK', 'FUNCTION', 'SCOPE', 'LOOP', 'GIT'];
-//var puzzleHint = ["This midwestern state is known for cheese curds.", "This French artist is most widely known for his paintings of ballerinas. (HINT: Last name only)", "This classic Chevrolet was originally known as the Opel.", "Name the artist who wrote the Bangles classic hit, Manic Monday.", "Not Jobs. Name the other Steve who founded Apple.", "If you took part in this social media site, Tom was probably your first friend.", "Name the 70s German electronic act, known for albums 'Computer Love' and 'It's More Fun to Compute,' are still performing 3D concerts.", "When released in 2009, this movie surpassed 'Titanic' as the all-time highest-grossing movie worldwide.", "This type of word can be spelled the same way backwards and forwards.", "This is made up of 8 bits."]
-//var puzzleHintChoice = puzzleHint(Math.floor(Math.random()*10));
-//var hint = puzzleHint[puzzleHintChoice];
-//var puzzleAnswerChoice = puzzleAnswer(Math.floor(Math.random()*10));
-//Original code from video before
-var choice = Math.floor(Math.random()*10);
-var answer = game[choice];
-var myLength = answer.length;
-var display = [myLength];
-var win = myLength;
-var attemptsLeft = 10;
-var letters = answer.split('');
-var output="";
-var letterChoice="";
-//alert(answer);*/
-
-var setUp = function(){
-for (var i=0; i < this.answer.length; i++){
-		display[i] = "_ ";
-		output = output + display[i];
+function Game(){
+	this.words = ['ARGUMENT', 'THIS', 'CONCATENATE', 'STRING', 'INTEGER', 'CALLBACK', 'FUNCTION', 'SCOPE', 'LOOP', 'GIT'];
+	this.choice = Math.floor(Math.random()*10);
+	this.answer = this.words[this.choice];
+	this.myLength = this.answer.length;
+	this.display = [this.myLength];
+	this.win = this.myLength;
+    this.attemptsLeft = 10;
+	this.letters = this.answer.split('');
+	this.output="";
+	this.letterChoice = "";
+	this.guesses = [];
+	this.spinWheel;
+	this.start = function(){
+		console.log('game started');
+		this.start_wheel();
+		this.draw_board();
 	}
-	//document.getElementById("clue").innerHTML = display[i];
-	document.getElementById("letterChoice").innerHTML= output;
-	output="";
-};
-
-var submit = function (){
-
-	output="";
-	letterChoice = $('#letterField').val();
-	$('#letterField').value="";
-
-	for (var c=0; c < answer.length; c++){
-		//debugger;
-		if (letterChoice == letters[c]){
-
-			display[c] = letterChoice;
-			win--;
-		} else {
-
+	this.draw_board = function(){
+		console.log(this.answer);
+		for (var i=0; i < this.answer.length; i++){
+			if(this.guesses.indexOf(this.answer[i]) == -1 ){
+				//this.display[i] = "_ ";
+				this.display[i] = '<div class="tile"><h1>&nbsp;</h1></div>'
+			} else {
+				this.display[i] = this.answer[i] + " ";
+			}
+			this.output += this.display[i];
 		}
-		output = output + display[c] + " ";
+		//document.getElementById("clue").innerHTML = display[i];
+		document.getElementById("letterChoice").innerHTML = this.output;
+		this.output = "";		
 	}
-	document.getElementById("letterChoice").innerHTML = output;
-	output="";
-	attemptsLeft--;
+	this.start_wheel = function(){
+		this.spinWheel = new Winwheel({
+			'canvasId' : 'myCanvas',
+			'linewidth' : 10, 
+			'numSegments' : 7,
+			'textAlignment' : 'outer',
+			'textMargin': 15, 
+			'textFontSize': 36,  
+			'segments' :
+			[
+				{'fillStyle' : '#669999', 'text' : 'Vowel'},
+				{'fillStyle' : '#89f26e', 'text' : 'Consonant'},
+				{'fillStyle' : '#7de6ef', 'text' : 'Vowel'},
+				{'fillStyle' : '#e7706f', 'text' : 'Consonant'}, 
+				{'fillStyle' : '#cc66ff', 'text' : 'Vowel'}, 
+				{'fillStyle' : '#ffff66', 'text' : 'Consonant'}, 
+				{'fillStyle' : '#ac7339', 'text' : 'Consonant'}
+			],
 
-	if (win < 1) {
-		document.getElementById("guesses").innerHTML = "You Win!";
-	} else if (attemptsLeft < 1) {
-		document.getElementById("guesses").innherHTML = "You Lose."; 
-
-	} else if (attemptsLeft > 1) {
-		document.getElementById("guesses").innherHTML = "You have " + attemptsLeft + " guesses left";
-	} else {
-		document.getElementById("guesses").innerHTML = "Choose another letter that is not in the "
+			'animation': 
+				{
+					'type' : 'spinToStop', 
+					'duration' : 5, 
+					'spins': 8
+				} 
+		});
 	}
+
+	this.guess = function(){
+		this.output=""; 
+		this.letterChoice = $('#letterField').val(); 
+		console.log('guessing ' + this.letterChoice);
+		this.guesses.push(this.letterChoice);
+		$('#letterField').val("");
+		this.draw_board();
+
+
+		//This should be its own Function
+		// for (var c=10; c < this.answer.length; c++){
+		// 	if (this.letterChoice == this.letters[c]){
+		// 		this.display[c] = this.letterChoice;
+		// 		this.win--;
+		// 	} else {
+		// 		this.output += this.display[c] + " ";
+		// 	}
+		// 	document.getElementById("letterChoice").innerHTML = this.output; 
+		// 	this.output=""; 
+		// 	this.attemptsLeft--;
+
+		// 	if (this.win < 1) {
+		// 		document.getElementById('guesses').innerHTML = 'You Win!';
+		// 	} else if (this.attemptsLeft < 1) {
+		// 		document.getElementById('guesses').innerHTML = 'You Lose. Better Luck Next Time.';
+		// 	} else if (this.attemptsLeft > 1) {
+		// 		document.getElementById("guesses").innerHTML = "'Spin again. You have ' + attemptsLeft + 'guesses left.'";  
+		// 	} else {
+		// 		document.getElementById("guesses").innerHTML = "Oops! That letter is not in the phrase. Spin again!"
+		// 	}
+		// }	
+
+	};
+
 }
 
+// var words = ['ARGUMENT', 'THIS', 'CONCATENATE', 'STRING', 'INTEGER', 'CALLBACK', 'FUNCTION', 'SCOPE', 'LOOP', 'GIT'];
+// //var puzzleHint = ["This midwestern state is known for cheese curds.", "This French artist is most widely known for his paintings of ballerinas. (HINT: Last name only)", "This classic Chevrolet was originally known as the Opel.", "Name the artist who wrote the Bangles classic hit, Manic Monday.", "Not Jobs. Name the other Steve who founded Apple.", "If you took part in this social media site, Tom was probably your first friend.", "Name the 70s German electronic act, known for albums 'Computer Love' and 'It's More Fun to Compute,' are still performing 3D concerts.", "When released in 2009, this movie surpassed 'Titanic' as the all-time highest-grossing movie worldwide.", "This type of word can be spelled the same way backwards and forwards.", "This is made up of 8 bits."]
+// //var puzzleHintChoice = puzzleHint(Math.floor(Math.random()*10));
+// //var hint = puzzleHint[puzzleHintChoice];
+// //var puzzleAnswerChoice = puzzleAnswer(Math.floor(Math.random()*10));
+// //Original code from video before
+// var choice = Math.floor(Math.random()*10);
+// var answer = words[choice];
+// var myLength = answer.length;
+// var display = [myLength];
+// var win = myLength;
+// var attemptsLeft = 10;
+// var letters = answer.split('');
+// var output="";
+// var letterChoice="";
+// //alert(answer);*/
+
+// var setUp = function(){
+// for (var i=0; i < this.answer.length; i++){
+// 		display[i] = "_ ";
+// 		output = output + display[i];
+// 	}
+// 	//document.getElementById("clue").innerHTML = display[i];
+// 	document.getElementById("letterChoice").innerHTML= output;
+// 	output="";
+// };
+
+// var submit = function (){
+
+// 	output=""; 
+// 	letterChoice = $('#letterButton').val(); 
+// 	$('#letterButton').value="";
+
+// 	for (var c=10; c < answer.length; c++){
+// 		if (letterChoice == letters[c]){
+// 			display[c] = letterChoice;
+// 			win--;
+// 		} else {
+// 			output += display[c] + " ";
+// 		}
+// 		document.getElementById("letterChoice").innerHTML = output; 
+// 		output=""; 
+// 		attemptsLeft--;
+
+// 		if (win < 1) {
+// 			document.getElementById('guesses').innerHTML = 'You Win!';
+// 		} else if (attemptsLeft < 1) {
+// 			document.getElementById('guesses').innerHTML = 'You Lose. Better Luck Next Time.';
+// 		} else if (attemptsLeft > 1) {
+// 			document.getElementById("guesses").innerHTML = "'Spin again. You have ' + attemptsLeft + 'guesses left.'";  
+// 		} else {
+// 			document.getElementById("guesses").innerHTML = "Oops! That letter is not in the phrase. Spin again!"
+// 		}
+// 	}	
+
+// };
 
 
- 
 
 
 
